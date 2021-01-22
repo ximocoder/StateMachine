4cfb923b58c45302020a4b428150a29196386497
ximocoder <ximo.coder@gmail.com>
2021-01-22 22:27:30 +0100

    Add net console project

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppStateMachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
