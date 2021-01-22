9f17e50f5634395efd9e89e74ea47bb171aa8660
ximocoder <ximo.coder@gmail.com>
2021-01-22 22:35:41 +0100

    Add phone sample

--- src/Program.cs
+++ src/Program.cs
@@ -6,7 +6,36 @@ namespace ConsoleAppStateMachine
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            TestState();
+        }
+
+        static void TestState()
+        {
+            var phoneCall = new PhoneCall("Lokesh");
+
+            phoneCall.Print();
+            phoneCall.Dialed("Prameela");
+            phoneCall.Print();
+            phoneCall.Connected();
+            phoneCall.Print();
+            phoneCall.SetVolume(2);
+            phoneCall.Print();
+            phoneCall.Hold();
+            phoneCall.Print();
+            phoneCall.Mute();
+            phoneCall.Print();
+            phoneCall.Unmute();
+            phoneCall.Print();
+            phoneCall.Resume();
+            phoneCall.Print();
+            phoneCall.SetVolume(11);
+            phoneCall.Print();
+
+
+            Console.WriteLine(phoneCall.ToDotGraph());
+
+            Console.WriteLine("Press any key...");
+            Console.ReadKey(true);
         }
     }
 }
